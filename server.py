a8166a55eb2cdf0a9ea9d9bb361ef52dfd4e79f9
awscloudvendor1 <83281099+awscloudvendor1@users.noreply.github.com>
2022-10-12 14:40:43 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hello ALL I hope u understand AppRunner, " + name + "!\n"
+    message = "I hope Everyone Understood the AppRuuner Implementation, " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
